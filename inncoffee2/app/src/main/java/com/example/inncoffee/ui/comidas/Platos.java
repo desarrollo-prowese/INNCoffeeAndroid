2b15cb3a7455c5715c0c2ab5324b07f760dfee0c
Kikote300 <kikote300@hotmail.com>
2020-04-12 03:14:20 +0200

    Fixed

--- inncoffee2/app/src/main/java/com/example/inncoffee/ui/comidas/Platos.java
+++ inncoffee2/app/src/main/java/com/example/inncoffee/ui/comidas/Platos.java
@@ -199,6 +199,7 @@ public class Platos extends Fragment {
                                     String precio = precios;
                                     MisPedidosClass user2 = new MisPedidosClass(texto, precio);
                                     mUsuario.child("PedidosSinFinalizarComidas").child(ID).child(key3).setValue(user2);
+                                    mUsuario.child("PedidosFinalizados").child(ID).child(key3).setValue(user2);
 
                                 }else if (contador2 > 1){
                                     String texto = contador2 + " /" + media.getText() + "/" + nombre;
@@ -211,6 +212,7 @@ public class Platos extends Fragment {
                                     String precio = processed;
                                     MisPedidosClass user2 = new MisPedidosClass(texto, precio);
                                     mUsuario.child("PedidosSinFinalizarComidas").child(ID).child(key3).setValue(user2);
+                                    mUsuario.child("PedidosFinalizados").child(ID).child(key3).setValue(user2);
                                 }
 
                             }
